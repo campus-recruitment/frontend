6eecadbf022a05dcfca206db556a95c07496b7ff
Dipti Rani <gdipti642@gmail.com>
2022-04-08 09:31:36 +0530

    feat: application page

--- src/components/dashboard/Applications.js
+++ src/components/dashboard/Applications.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Box } from '@mui/material';
+import { Box, Grid, Card, CardContent, Typography, CardActions, Button } from '@mui/material';
+import { LocationOn, PlayCircleOutline, Money, DateRange, WorkOutline, ArrowForwardIos } from '@mui/icons-material';
 import { UserContext } from '../../contexts/userContext';
 
 export default function Applications() {
@@ -7,22 +8,95 @@ export default function Applications() {
     const [visitors, setVisitors] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/api/visitor', {
+        fetch(`http://localhost:5000/api/student/${user.user_id}`, {
             method: 'GET',
             headers: {
                 'Authorization': 'Bearer ' + user.token
             }
         }).then(res => res.json())
             .then(data => {
-                console.log(data.visitor);
-                setVisitors(data.visitor);
+                console.log(data.student);
+                setVisitors(data.student.appliedVisitors);
             })
     }, [])
-    return (
+    return ( 
         <>
             <Box sx={{ mt: 2, height: '95%', border: '2px solid #c8c7c7', borderRadius: '6px' }}>
-                Hello
-            </Box>
+                    <Grid container>
+                        <Grid item lg={8} md={8} sm={12} xs={12}>
+                            {visitors?.map((vis, i) => (  
+                                <Card sx={{ m: 2 }} key={i}>
+                                    <CardContent>
+                                        <Typography variant="h6" component="div">
+                                            {vis.positionName}
+                                        </Typography>
+                                        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                                {vis.companyName}
+                                            </Typography>
+                                            <Typography variant="body2" color="text.secondary">
+                                                <LocationOn sx={{ fontSize: '1.2em' }} />{vis.location}
+                                            </Typography>
+                                        </Box>
+                                        <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 1 }}>
+                                            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                                    <PlayCircleOutline sx={{ fontSize: '1.2em', mr: 0.5 }} />Start Date
+                                                </Typography>
+                                                <Typography sx={{ fontSize: 14 }} gutterBottom>
+                                                    18/03/2022
+                                                </Typography>
+                                            </Box>
+                                            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                                    <Money sx={{ fontSize: '1.2em', mr: 0.5 }} />Package
+                                                </Typography>
+                                                <Typography sx={{ fontSize: 14 }} gutterBottom>
+                                                    {vis.packages}
+                                                </Typography>
+                                            </Box>
+                                            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                                    <DateRange sx={{ fontSize: '1.2em', mr: 0.5 }} />Apply By
+                                                </Typography>
+                                                <Typography sx={{ fontSize: 14 }} gutterBottom>
+                                                    {vis.dueDate}
+                                                </Typography>
+                                            </Box>
+                                            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                                    <WorkOutline sx={{ fontSize: '1.2em', mr: 0.5 }} />Vacancies
+                                                </Typography>
+                                                <Typography sx={{ fontSize: 14 }} gutterBottom>
+                                                    {vis.vacancies}
+                                                </Typography>
+                                            </Box>
+                                        </Box>
+                                    </CardContent>
+                                    <CardActions sx={{ display: 'flex', justifyContent: 'space-between', mt: -1 }}>
+                                        {vis.fullTime ?
+                                            <Typography sx={{ fontSize: 14, ml: 1 }} color="text.secondary" gutterBottom>
+                                                Full-time
+                                            </Typography>
+                                            :
+                                            <Typography sx={{ fontSize: 14, ml: 1 }} color="text.secondary" gutterBottom>
+                                                Part-time
+                                            </Typography>
+                                        }
+                                        {/* <Button size="small" sx={{
+                                            textTransform: 'none',
+                                            color: '#051846',
+                                            fontFamily: "Poppins",
+                                            fontWeight: 900,
+                                            fontSize: 14
+                                        }}>View Details<ArrowForwardIos sx={{ fontSize: '1.2em' }} /></Button> */}
+                                    </CardActions>
+                                </Card>
+                            ))}
+                        </Grid>
+                        <Grid item lg={4} md={4} sm={12} xs={12}></Grid>
+                    </Grid>
+                </Box>
         </>
     )
 }
